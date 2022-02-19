92c670ab1f431acd96ffc502ae56d9a46814727d
Adham Gamal <adhammohamadgamal@yahoo.com>
2022-02-19 10:20:39 +0200

    update variables' titles

--- src/main/kotlin/TheatreSquare.kt
+++ src/main/kotlin/TheatreSquare.kt
@@ -1,48 +1,28 @@
 import java.util.Scanner
 
-/**
- * A. Theatre Square
- *
- * time limit per test: 1 second
- *
- * memory limit per test : 256 megabytes
- *
- * input : standard input
- *
- * output : standard output
- *
- * Theatre Square in the capital city of Berland has a rectangular shape with the size n × m meters. On the occasion of the city's anniversary, a decision was taken to pave the Square with square granite flagstones. Each flagstone is of the size a × a.
- *
- * What is the least number of flagstones needed to pave the Square? It's allowed to cover the surface larger than the Theatre Square, but the Square has to be covered. It's not allowed to break the flagstones. The sides of flagstones should be parallel to the sides of the Square.
- *
- * Input :
- *
- * The input contains three positive integer numbers in the first line: n, m and a (1 ≤ n, m, a ≤ 109).
- *
- * Examples : 6 6 4
- *
- * Output :
- *
- * Write the needed number of flagstones.
- *
- * Examples : 4
- **/
-
 fun main() {
     val read = Scanner(System.`in`)
-    val n = read.nextLong()
-    val m = read.nextLong()
-    val a = read.nextLong()
-    val x = if ((n.toDouble() / a) % 1.0 != 0.0) {
-        (n / a) + 1
+
+    val theatreWidth = read.nextLong()
+    val theatreHeight = read.nextLong()
+    val flagstoneLength = read.nextLong()
+
+    val widthFractions = theatreWidth % flagstoneLength
+    val heightFractions = theatreHeight % flagstoneLength
+
+    val numOfFlagstonesNeededToFillWidth = if (widthFractions > 0) {
+        (theatreWidth / flagstoneLength) + 1
     } else {
-        (n / a)
-    }
-    val y = if ((m.toDouble() / a) % 1.0 != 0.0) {
-        (m / a) + 1
-    } else {
-        (m / a)
+        (theatreWidth / flagstoneLength)
     }
 
-    println(x*y)
+    val numOfFlagstonesNeededToFillHeight = if (heightFractions > 0) {
+        (theatreHeight / flagstoneLength) + 1
+    } else {
+        (theatreHeight / flagstoneLength)
+    }
+
+    val totalNumberOfFlagstones = numOfFlagstonesNeededToFillWidth * numOfFlagstonesNeededToFillHeight
+
+    println(totalNumberOfFlagstones)
 }
